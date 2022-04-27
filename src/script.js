66bb4333cf0a19c08ce7669a651f055c986b6934
Oluwasanmi Idowu <95680393+helloSanmi@users.noreply.github.com>
2022-04-27 20:37:59 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,67 +1,58 @@
-//custom event
-    const event = new Event('onTodoDatabaseChange');
+    //custom event
+    //const event = new Event('onTodoDatabaseChange');
 
-    var todoInput = document.getElementById("todoInput");
-    var todoButton = document.getElementById("todoBtn");
-    var listContainer = document.getElementById("todoList");
+    var todoInput = document.getElementById('todoInput');
+    var todoButton = document.getElementById('todoBtn');
+    var listContainer = document.getElementById('todoList');
+    var output = document.getElementById('output');
     var todoDatabase = [];
 
-    document.addEventListener("onTodoDatabaseChange", function (e) {
-        display();
-      }, false);
+    // document.addEventListener('onTodoDatabaseChange', function(e){
+    //     display();
+    // }, false);
 
-    function display() {
-      listContainer.innerHTML = "";
-      todoDatabase.forEach(function (val, index) {
-        var content =
+    function display(){
+        listContainer.innerHTML = "";
+        todoDatabase.forEach(function(val, index){
+          var content =
           '<li class="list-group-item list-group-item-action text-start" aria-current="true">' +
-          "<span>" +
-          val +
-          '</span>  <a href="#" onclick="removeElement(event)" id="' +
-          index +
-          '" class="text-end">Delete</a>' +
-          "</li>";
+          "<span>" +val+ '</span>  <a href="#" onclick="removeElement(event)" id="' 
+          +index+ '" class="text-end">Delete</a></li>';
 				listContainer.innerHTML += content;
-			});
-			todoInput.value = "";
-      console.log(todoDatabase);
+        });
+        todoInput.value = "";
     }
 
-    function removeElement(event) {
-      var id = event.target.getAttribute("id");
-      //remove array content
-      todoDatabase.splice(id, 1);
-      event.target.parentNode.remove();
+    function removeElement(event){
+        var id = event.target.getAttribute('id');
+        //remove array content
+        todoDatabase.splice(id, 1);
+        event.target.parentNode.remove();
     }
 
-    todoInput.addEventListener("keypress", function (event) {
-      var value = event.target.value;
-      if (value.length > 0 && event.which === 13) {
-        if (todoDatabase.includes(value) === false) {
-          todoDatabase.push(value);
-        }
-        else{
-          alert('Value already added to the List')
-        }
-        // todoInput.dispatchEvent(event);
-        display();
+    function checkDuplicate(val){
+      output.innerHTML = ""
+      if (todoDatabase.includes(val) === false) {
+        var message = todoDatabase.push(val);
       }
-      
-		});
-		
-    todoButton.addEventListener("click", function () {
-      var value = todoInput.value;
-      if (value.length > 0) {
-        if (todoDatabase.includes(value) === false) {
-          todoDatabase.push(value);
-        }
-        else{
-          alert('Value already added to the List')
-        }
-        // todoButton.dispatchEvent(event);;
-        display();
+      else{
+        var message = output.innerHTML += '<strong>Oops! Todo already added</strong>'
       }
+      return message;
+    }
+
+    todoInput.addEventListener('keypress', function(event){
+        var value = event.target.value;
+        if(value.length > 0 && event.which === 13){
+            checkDuplicate(value)
+            display();
+        }
     });
-    
-      
-   
+
+    todoButton.addEventListener('click', function(){
+        var value = todoInput.value;
+        if(value.length > 0){
+          checkDuplicate(value)
+            display();
+        }
+    })
